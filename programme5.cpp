c5b0bafb0cb0fcdd3ae988d360a607451dbcf47f
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-24 19:08:28 +0530

    Create programme5.cpp

---
+++ programme5.cpp
@@ -0,0 +1,11 @@
+//Lab1-problem5
+#include <iostream>
+using namespace std;
+//program 4
+int main() {
+	// your code goes here
+int l=12,b=4;
+char x='l',y='b',a='A';
+cout <<x<<'='<<l<<endl<<y<<'='<<b<<endl<<a<<'='<<l*b;
+	return 0;
+}
