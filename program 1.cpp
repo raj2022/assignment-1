de0acd06afeab58b0fa10d96f3b3dee5674d6d20
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-24 18:13:16 +0530

    Create program 1.cpp

---
+++ program 1.cpp
@@ -0,0 +1,16 @@
+@@ -0,0 +1,14 @@
+ //Lab1-Program 1
+ #include <iostream>
+ using namespace std;
+ 
+ 
+ int main() {
+ 	// your code goes here
+ 	char a='t';
+ 	int b=16;
+ 	float c=.04;
+ 	bool d=true;
+ 	cout<< a<<endl<<b<<endl<<c<<endl<<d;
+ 	
+ 	return 0;
+ }
