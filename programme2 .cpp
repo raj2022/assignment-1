57fce250267eef569ba2a6649c9a6786cb1b2a51
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-24 19:31:13 +0530

    Update programme2 .cpp

--- programme2 .cpp
+++ programme2 .cpp
@@ -1,1 +1,17 @@
+#include  <iostream>
+using  naespace std;
+/*C++  program to accept two numbers and print their sum.*/
 
+int main ()
+{
+  float a=0 , b=0;
+  float  sum;
+  cout<< "Hello there, welcome"<<end1;
+  cout<<"\n \nEnter the first number:  ";
+  cin>>a;
+  cout<<"\nEnter the second number:  ";
+  cin>>b;
+  sum=a+b;
+  cout<<"\n \nThe sum of the two numbers entered is : "<<sum<<end1;
+  return  0:
+}
