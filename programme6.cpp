cc8533e5f293227dc68e48b743e33c6ab2e1a628
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-24 19:16:43 +0530

    Create programme6.cpp

---
+++ programme6.cpp
@@ -0,0 +1,12 @@
+//Lab1-problem6
+#include <iostream>
+using namespace std;
+//program 4
+int main() {
+	// your code goes here
+int r=5,d=2*r;
+double p=2*3.14*r,a=3.14*r*r;
+char x='R',y='D',z='P',w='A';
+cout <<x<<'='<<r<<endl<<y<<'='<<d<<endl<<z<<'='<<p<<endl<<w<<'='<<a;
+	return 0;
+}
