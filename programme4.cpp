6e3b3dd342fe14399a5b4e1eba385c24f221d8fd
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-24 19:01:08 +0530

    Create programme4.cpp

---
+++ programme4.cpp
@@ -0,0 +1,10 @@
+//problem4
+#include <iostream>
+using namespace std;
+int main() {
+	// your code goes here
+int l=16,b=8;
+char x='2',y='b',a='P';
+cout <<x<<'='<<l<<endl<<y<<'='<<b<<endl<<a<<'='<<2*(l+b);
+	return 0;
+}
