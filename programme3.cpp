bff89bd10d793a5a16c9c26f37b508bf8c2230e9
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-24 18:56:10 +0530

    Update programme3.cpp

--- programme3.cpp
+++ programme3.cpp
@@ -1,1 +1,12 @@
+//Lab1-Program 3
+#include <iostream>
+using namespace std;
 
+int main() {
+	// your code goes here
+	char a='a',s='s',m='m',d='d',x='x',y='y';
+	int p=3,q=9,r=p+q,v=p-q,t=p*q,u=q/p;
+	cout <<x<<" "<<p<<endl<<y<<" "<<q<<endl<<a<<" "<<r<<endl<<s<<" "<<v<<endl<<m<<" "<<t<<endl<<d<<" "<<u;
+	
+	return 0;
+}
